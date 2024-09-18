19339794bfcd2f39abdd3ae3ecb9f8e3addcb84b
Rajan Valencia <47655366+rajanvalencia@users.noreply.github.com>
2024-09-18 22:38:34 +0900

    refactor: CheckoutSessionController

--- app/Http/Controllers/CheckoutSessionController.php
+++ app/Http/Controllers/CheckoutSessionController.php
@@ -14,38 +14,34 @@ class CheckoutSessionController extends Controller
 
     public function create(DonationFormRequest $request): JsonResponse
     {
-//        Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+        // Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
 
         $data = $request->validated();
-        // Initialize strip
+
         try {
-            // Generate a custom external_id for tracking
             $externalId = Helpers::generateUuid();
 
-            // Create the customer in Stripe
-            // pass data['customer'] , external_id
             $stripeCustomer = StripeProvider::createCustomer($data['customer'] , $externalId);
-            // Store the donor info of req and stripe customer with created externalId
-            $donor = DonorRepository::storeDonor( $data , $stripeCustomer , $externalId);
+            $donor = DonorRepository::storeDonor($data, $stripeCustomer , $externalId);
 
-            $price = StripeProvider::createPrice($data['product_id'], $data['price']);
-//
-            $checkoutSession = StripeProvider::createCheckoutSession($stripeCustomer->id, $price->id);
+            $stripePrice = StripeProvider::createPrice($data['product_id'], $data['price']);
 
+            $checkoutSession = StripeProvider::createCheckoutSession($stripeCustomer->id, $stripePrice->id);
 
-
+            $data = [
+                "donor" => $donor,
+                "stripe_checkout_session" => $checkoutSession,
+                'stripe_price' => $stripePrice,
+                "stripe_customer" => $stripeCustomer,
+            ];
 
             return response()->json([
-                'createdPrice' => $price,
-                "donor" => $donor,
-                "id" => $checkoutSession->id,
-                'priceId' => $price->id,
-                "checkout-session" => $checkoutSession,
-                "stripe Customer" => $stripeCustomer,
-            ]);
+                'status' => 201,
+                'message' => 'success',
+                'data' => $data,
+            ], 201);
 
         } catch (Exception $e) {
-            // Log the error and return a 500 error response
             return response()->json(['error' => $e->getMessage() , "error detail" => $e->getFile()], 500);
         }
     }
