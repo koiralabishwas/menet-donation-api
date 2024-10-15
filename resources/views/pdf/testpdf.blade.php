71ce73dc0d81977d2c6e75f9b952e628d6abfee3
Bishwas Koirala <wasubisu69@gmail.com>
2024-10-15 22:11:26 +0900

    fixed : corporate number  not rendering bug

--- resources/views/pdf/testpdf.blade.php
+++ resources/views/pdf/testpdf.blade.php
@@ -81,7 +81,7 @@
     <p>寄附者ID: {{ $donor['donor_external_id'] }}</p>
     <p>寄附者住所: {{ $donor['address'] }}</p>
     <p>寄附者氏名または法人名: {{ $donor['name'] }} 様</p>
-    <p>寄附者法人番号: {{ $donor['corporation_id'] }}</p>
+    <p>寄附者法人番号: {{ $donor['corporate_no'] }}</p>
     <p>年間寄附総額: <span class="bold">{{ number_format($total_amount) }}円</span></p>
 </div>
 
