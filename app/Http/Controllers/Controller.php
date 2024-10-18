5ac02ea4b896eaec9bbbe9436757bfc30df82292
Rajan Valencia <47655366+rajanvalencia@users.noreply.github.com>
2024-10-19 01:32:56 +0900

    feat: swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,16 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="HSGuide API",
+ * )
+ *
+ * @OA\PathItem(
+ *     path="/api/",
+ * )
+ */
 abstract class Controller
 {
     //
