c52c5c7617eae8e6c571338d8373fc5d80c264db
Rajan Valencia <47655366+rajanvalencia@users.noreply.github.com>
2024-10-25 10:44:14 +0900

    feat: laravel-enum

---
+++ app/Enums/ProductName.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class ProductName extends Enum
+{
+    const ALL = '全て';
+
+    const ALTERVOICE = 'オルタボイス';
+
+    const EDUCATIONAL_COUNSELING = '教育相談';
+
+    const PEOPLE_IN_NEED = '生活困窮者';
+
+    public static function getKeyByLowerCase(string $lowerKey): ?string
+    {
+        foreach (self::getKeys() as $key) {
+            if (strtolower($key) === $lowerKey) {
+                return self::getValue($key);
+            }
+        }
+
+        return null;
+    }
+}
