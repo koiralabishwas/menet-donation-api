388b77ec65ab194380b31fbde7efda97cde6846e
Rajan Valencia <47655366+rajanvalencia@users.noreply.github.com>
2024-11-07 21:47:43 +0900

    refactor: unused functions removed

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -6,16 +6,6 @@ use Illuminate\Support\Str;
 
 class Helpers
 {
-    public static function CreateExternalIdfromDate(): string
-    {
-        $fullYear = date('Y');
-        $date = date('md');
-
-        $randomPart = Str::upper(Str::random(6));
-
-        return "$fullYear$date-$randomPart";
-    }
-
     public static function createUuid(): string
     {
         return Str::uuid()->toString();
