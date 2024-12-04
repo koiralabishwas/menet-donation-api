c5a6f6fccba9d31b561289b496558165f53c432e
Rajan Valencia <47655366+rajanvalencia@users.noreply.github.com>
2024-12-05 02:15:58 +0900

    feat: custom exception

---
+++ app/Exceptions/CustomException.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+
+class CustomException extends Exception
+{
+    protected string $type;
+
+    protected string $userEmail;
+
+    protected $code;
+
+    protected int $line;
+
+    protected string $shortMessage;
+
+    protected $message;
+
+    public function __construct(
+        string $type,
+        string $userEmail,
+        string $shortMessage,
+        Exception $e
+    ) {
+        parent::__construct($e->getMessage(), $e->getCode(), $e);
+
+        $this->type = $type;
+        $this->userEmail = $userEmail;
+        $this->code = $e->getCode() ?: 500; // Default to 500 if no code is provided
+        $this->line = $e->getLine();
+        $this->shortMessage = $shortMessage;
+        $this->message = $e->getMessage();
+    }
+
+    public function render($request): JsonResponse
+    {
+        return response()->json([
+            'success' => false,
+            'timestamp' => now()->toDateTimeString(),
+            'route' => '/'.$request->path(),
+            'error_type' => $this->type,
+            'error_line' => $this->line,
+            'user_email' => $this->userEmail,
+            'short_message' => $this->shortMessage,
+            'message' => $this->message,
+        ], $this->code);
+    }
+}
