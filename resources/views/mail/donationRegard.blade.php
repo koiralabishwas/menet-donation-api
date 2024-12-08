654bfea8576500b921a6f00399db8926365f21ea
Rajan Valencia <47655366+rajanvalencia@users.noreply.github.com>
2024-12-08 16:46:08 +0900

    fix: email design

--- resources/views/mail/donationRegard.blade.php
+++ resources/views/mail/donationRegard.blade.php
@@ -1,10 +1,16 @@
+@component('mail::message')
 {{ $donorName }}様
 
 この度は、{{ $donationProject }}にご寄付いただき、誠にありがとうございます。
 
-寄付金額: ¥{{ number_format($donationAmount) }}
+@component('mail::table')
+    |         |                                        |
+    | :---    | :---                                   |
+    | 寄付金額 | ¥{{ number_format($donationAmount) }}円 |
+@endcomponent
 
 ご支援のおかげで、今後も{{$donationProject}}を欠かさず永続できます。
 
 寄付控除証明書は来年の1月にメールにて送信させていただきます。
 引き続きよろしくお願いいたします。
+@endcomponent
