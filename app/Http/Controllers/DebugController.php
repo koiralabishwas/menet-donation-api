bcd9d604e6ef540a40b2e431e5ff70aa42e4f3ef
Bishwas Koirala <wasubisu69@gmail.com>
2024-12-09 19:38:16 +0900

    迷走してしまっていた。
    to use webhook endpoint locally ,
    
     use stripe listen --forward-to localhost:8000/api/webhooks/webhookHandlerPath --events=webhookEventToCatch

--- app/Http/Controllers/DebugController.php
+++ app/Http/Controllers/DebugController.php
@@ -2,13 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\DonationRegardMailable;
 use App\Providers\StripeProvider;
+use App\Repositories\DonationRepository;
 use App\Repositories\DonorRepository;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Stripe\Exception\SignatureVerificationException;
+use Stripe\Webhook;
 
 class DebugController extends Controller
 {
+    /**
+     * @throws SignatureVerificationException
+     */
+    public function debugPaymentIntentSucceed(Request $request)
+    {
+        $endpoint_secret = env('STRIPE_WEBHOOK_SECRET');
+        $payload = $request->getcontent();
+        $sig_header = $request->header('Stripe-Signature');
+
+        $event = Webhook::constructEvent(
+            $payload, $sig_header, $endpoint_secret
+        );
+        switch ($event->type) {
+            // for one-time payment checkout session
+            case 'payment_intent.succeeded':   // to store one-time payment on db
+                $paymentIntent = $event->data;
+                $metaData = $paymentIntent['object']->metadata; // onetime のときのみここにmetadata 保存されている。
+                if ($metaData->type === 'ONE_TIME') {
+                    Log::info($metaData->type);
+                    Log::info($paymentIntent['object']); // テスㇳでつかうため　、必要
+                    //ここで残りのonetime時の処理
+                    DonationRepository::storeDonation($metaData, $paymentIntent['object']);
+                    Mail::to($paymentIntent['object']->receipt_email)->send(new DonationRegardMailable($metaData));
+                }
+                // catch and store subscription on db
+
+                return;
+
+            default:
+        }
+    }
+
     public function getStripeProductNameFromProductId(Request $request): JsonResponse
     {
         $productId = $request->query('product-id');
