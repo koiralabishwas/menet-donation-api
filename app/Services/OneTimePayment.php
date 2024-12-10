bbe1452695e5066779530bac2d1787601a656df2
Rajan Valencia <47655366+rajanvalencia@users.noreply.github.com>
2024-12-10 18:05:49 +0900

    refactor: checkout session controller

---
+++ app/Services/OneTimePayment.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Services;
+
+use App\Enums\StripeProductID;
+use App\Exceptions\CustomException;
+use App\Http\Requests\DonationFormRequest;
+use App\Providers\StripeProvider;
+use App\Repositories\DonorRepository;
+
+class OneTimePayment
+{
+    private $request;
+
+    private $formData;
+
+    private $donor;
+
+    private $stripeCustomer;
+
+    private $stripePrice;
+
+    private $paymentIntentMetaData;
+
+    public function __construct(DonationFormRequest $request)
+    {
+        $this->request = $request;
+    }
+
+    public function validate(): static
+    {
+        $this->formData = $this->request->validated();
+
+        return $this;
+    }
+
+    /**
+     * @throws CustomException
+     */
+    public function createCustomer(): static
+    {
+        $this->stripeCustomer = StripeProvider::createCustomer($this->formData['customer']);
+
+        return $this;
+    }
+
+    /**
+     * @throws CustomException
+     */
+    public function createPrice(): static
+    {
+        $this->stripePrice = StripeProvider::createOneTimePrice(
+            StripeProductID::getValueByLowerCaseKey($this->formData['product']),
+            $this->formData['price']
+        );
+
+        return $this;
+    }
+
+    /**
+     * @throws CustomException
+     */
+    public function createMetadata(): static
+    {
+        $this->donor = DonorRepository::storeDonor($this->formData, $this->stripeCustomer);
+        $this->donor['stripe_customer_object'] = json_decode($this->donor['stripe_customer_object']);
+
+        $this->paymentIntentMetaData = [
+            'donor_id' => $this->donor['donor_id'],
+            'donor_name' => $this->donor['name'],
+            'donor_external_id' => $this->donor['donor_external_id'],
+            'donor_type' => $this->donor['type'],
+            'donor_email' => $this->donor['email'],
+            'donation_project' => StripeProvider::getProductNameFromId(
+                StripeProductID::getValueByLowerCaseKey($this->formData['product'])
+            ),
+            'amount' => $this->formData['price'],
+            'currency' => 'jpy',
+            'type' => 'ONE_TIME',
+        ];
+
+        return $this;
+    }
+
+    /**
+     * @throws CustomException
+     */
+    public function createCheckoutSession(): array
+    {
+        $checkoutSession = StripeProvider::createCheckoutSession(
+            $this->stripeCustomer->id,
+            $this->stripePrice->id,
+            $this->paymentIntentMetaData
+        );
+
+        return [
+            'donor' => $this->donor,
+            'stripe_checkout_session' => $checkoutSession,
+            'stripe_price' => $this->stripePrice,
+            'stripe_customer' => $this->stripeCustomer,
+        ];
+    }
+}
