55e2926f2855ada7aed3ab8861572ce47cb05013
Rajan Valencia <47655366+rajanvalencia@users.noreply.github.com>
2024-12-15 16:24:41 +0900

    feat: global error handling

--- app/Providers/StripeProvider.php
+++ app/Providers/StripeProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Enums\LogType;
-use App\Exceptions\CustomException;
 use App\Helpers\EnvHelpers;
 use App\Helpers\Helpers;
 use App\Repositories\DonorRepository;
@@ -12,14 +10,12 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 use Stripe\Checkout\Session;
 use Stripe\Exception\ApiErrorException;
+use Stripe\Exception\InvalidRequestException;
 use Stripe\Price;
 use Stripe\StripeClient;
 
 class StripeProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     */
     public function register(): void
     {
         $this->app->singleton(StripeClient::class, function () {
@@ -28,82 +24,58 @@ class StripeProvider extends ServiceProvider
     }
 
     /**
-     * Bootstrap services.
-     */
-    public function boot(): void
-    {
-        //
-    }
-
-    /**
-     * Create a Stripe customer.
-     *
-     * @throws CustomException
+     * @throws ApiErrorException
+     * @throws InvalidRequestException
+     * @throws Exception
      */
     public static function createCustomer(array $customerData): object
     {
-        try {
-            $stripe = app(StripeClient::class);
+        $stripe = app(StripeClient::class);
 
-            // db から取得して返したほうが確実。ストライプから取得すると、作成した直後は帰ってこない
-            $existingDonor = DonorRepository::getDonorByEmail($customerData['email']);
+        // db から取得して返したほうが確実。ストライプから取得すると、作成した直後は帰ってこない
+        $existingDonor = DonorRepository::getDonorByEmail($customerData['email']);
 
-            if ($existingDonor) {
-                return $existingDonor;
-            }
-            $externalId = Helpers::createUuid();
-
-            return $stripe->customers->create([
-                'name' => $customerData['name'],
-                'email' => $customerData['email'],
-                'phone' => $customerData['phone'],
-                'address' => [
-                    'country' => $customerData['address']['country'],
-                    'postal_code' => $customerData['address']['postal_code'],
-                    'city' => $customerData['address']['city'],
-                    'line1' => $customerData['address']['line1'],
-                    'line2' => $customerData['address']['line2'],
-                ],
-                'metadata' => [
-                    'donor_external_id' => $externalId,
-                    'type' => $customerData['type'],
-                ],
-            ]);
-        } catch (Exception $e) {
-            throw new CustomException(
-                LogType::FATAL,
-                __LINE__,
-                $customerData['email'],
-                'Failed to create Stripe customer',
-                $e
-            );
+        if ($existingDonor) {
+            return $existingDonor;
         }
+        $externalId = Helpers::createUuid();
+
+        return $stripe->customers->create([
+            'name' => $customerData['name'],
+            'email' => $customerData['email'],
+            'phone' => $customerData['phone'],
+            'address' => [
+                'country' => $customerData['address']['country'],
+                'postal_code' => $customerData['address']['postal_code'],
+                'city' => $customerData['address']['city'],
+                'line1' => $customerData['address']['line1'],
+                'line2' => $customerData['address']['line2'],
+            ],
+            'metadata' => [
+                'donor_external_id' => $externalId,
+                'type' => $customerData['type'],
+            ],
+        ]);
     }
 
     /**
-     * @throws CustomException
+     * @throws ApiErrorException
+     * @throws InvalidRequestException
+     * @throws Exception
      */
     public static function searchCustomerFromEmail(string $email)
     {
-        try {
-            $stripe = app(StripeClient::class);
+        $stripe = app(StripeClient::class);
 
-            return $stripe->customers->search([
-                'query' => 'email:\''.$email.'\'',
-            ]);
-        } catch (Exception $e) {
-            throw new CustomException(
-                LogType::FATAL,
-                __LINE__,
-                $email,
-                'Failed to search Stripe customer.',
-                $e
-            );
-        }
+        return $stripe->customers->search([
+            'query' => 'email:\''.$email.'\'',
+        ]);
     }
 
     /**
-     * @throws CustomException
+     * @throws ApiErrorException
+     * @throws InvalidRequestException
+     * @throws Exception
      */
     public static function deleteAllCustomers(): string
     {
@@ -111,262 +83,185 @@ class StripeProvider extends ServiceProvider
             return 'Production environment is not allowed to delete all customers';
         }
 
-        try {
-            $stripe = app(StripeClient::class);
-            $count = 0;
-            while ($count < 6) {
-                $customers = $stripe->customers->all(['limit' => 100]);
-                foreach ($customers as $customer) {
-                    try {
-                        $stripe->customers->delete($customer->id);
-                    } catch (ApiErrorException $e) {
-                        Log::error("failed $customer->id &&& $e");
-                    }
+        $stripe = app(StripeClient::class);
+        $count = 0;
+        while ($count < 6) {
+            $customers = $stripe->customers->all(['limit' => 100]);
+            foreach ($customers as $customer) {
+                try {
+                    $stripe->customers->delete($customer->id);
+                } catch (ApiErrorException $e) {
+                    Log::error("failed $customer->id &&& $e");
                 }
-                $count++;
             }
-
-            return $count.' customers deleted';
-        } catch (Exception $e) {
-            throw new CustomException(
-                LogType::ERROR,
-                __LINE__,
-                '',
-                'Failed to delete all Stripe customers',
-                $e
-            );
+            $count++;
         }
+
+        return $count.' customers deleted';
     }
 
     /**
-     * @throws CustomException
+     * @throws ApiErrorException
+     * @throws InvalidRequestException
+     * @throws Exception
      */
     public static function searchOneTimePrice(string $productId, int $amount)
     {
-        try {
-            $stripe = app(StripeClient::class);
-            $existingPrice = $stripe->prices->search([
-                'query' => "active:\"true\" AND product:\"$productId\" AND type:\"one_time\" AND metadata[\"amount\"]:\"$amount\"",
-            ]);
-            if ($existingPrice->data) {
-                return $existingPrice->data[0];
-            }
-
-            return null;
-        } catch (Exception $e) {
-            throw new CustomException(
-                LogType::ERROR,
-                __LINE__,
-                '',
-                'Failed to search Stripe one-time price',
-                $e
-            );
+        $stripe = app(StripeClient::class);
+        $existingPrice = $stripe->prices->search([
+            'query' => "active:\"true\" AND product:\"$productId\" AND type:\"one_time\" AND metadata[\"amount\"]:\"$amount\"",
+        ]);
+        if ($existingPrice->data) {
+            return $existingPrice->data[0];
         }
+
+        return null;
     }
 
     /**
-     * @throws CustomException
+     * @throws ApiErrorException
+     * @throws InvalidRequestException
+     * @throws Exception
      */
     public static function searchSubscriptionPrice(string $productId, int $amount)
     {
-        try {
-            $stripe = app(StripeClient::class);
-            $existingPrice = $stripe->prices->search([
-                'query' => "active:\"true\" AND product:\"$productId\" AND type:\"recurring\" AND metadata[\"amount\"]:\"$amount\"",
-            ]);
-            if ($existingPrice->data) {
-                return $existingPrice->data[0];
-            }
-
-            return null;
-        } catch (Exception $e) {
-            throw new CustomException(
-                LogType::ERROR,
-                __LINE__,
-                '',
-                'Failed to search Stripe subscription price',
-                $e
-            );
+        $stripe = app(StripeClient::class);
+        $existingPrice = $stripe->prices->search([
+            'query' => "active:\"true\" AND product:\"$productId\" AND type:\"recurring\" AND metadata[\"amount\"]:\"$amount\"",
+        ]);
+        if ($existingPrice->data) {
+            return $existingPrice->data[0];
         }
+
+        return null;
     }
 
     /**
-     * @throws CustomException
+     * @throws ApiErrorException
+     * @throws InvalidRequestException
+     * @throws Exception
      */
     public static function createOneTimePrice(string $productId, int $amount): Price
     {
-        try {
-            $stripe = app(StripeClient::class);
+        $stripe = app(StripeClient::class);
 
-            $existingPrice = self::searchOneTimePrice($productId, $amount);
+        $existingPrice = self::searchOneTimePrice($productId, $amount);
 
-            if ($existingPrice) {
-                return $existingPrice;
-            }
-
-            return $stripe->prices->create([
-                'product' => $productId,
-                'currency' => 'jpy',
-                'unit_amount' => $amount,
-                'metadata' => ['amount' => $amount],
-            ]);
-        } catch (Exception $e) {
-            throw new CustomException(
-                LogType::FATAL,
-                __LINE__,
-                '',
-                'Failed to create Stripe one-time price',
-                $e
-            );
+        if ($existingPrice) {
+            return $existingPrice;
         }
+
+        return $stripe->prices->create([
+            'product' => $productId,
+            'currency' => 'jpy',
+            'unit_amount' => $amount,
+            'metadata' => ['amount' => $amount],
+        ]);
     }
 
     /**
-     * @throws CustomException
+     * @throws ApiErrorException
+     * @throws InvalidRequestException
+     * @throws Exception
      */
     public static function createSubscriptionPrice(string $productId, int $amount)
     {
-        try {
-            $stripe = app(StripeClient::class);
-            $existingPrice = self::searchSubscriptionPrice($productId, $amount);
-            if ($existingPrice) {
-                return $existingPrice;
-            }
-
-            return $stripe->prices->create([
-                'product' => $productId,
-                'currency' => 'jpy',
-                'unit_amount' => $amount,
-                'recurring' => ['interval' => 'month'],
-                'metadata' => ['amount' => $amount],
-            ]);
-        } catch (Exception $e) {
-            throw new CustomException(
-                LogType::FATAL,
-                __LINE__,
-                '',
-                'Failed to create Stripe subscription price',
-                $e
-            );
+        $stripe = app(StripeClient::class);
+        $existingPrice = self::searchSubscriptionPrice($productId, $amount);
+        if ($existingPrice) {
+            return $existingPrice;
         }
+
+        return $stripe->prices->create([
+            'product' => $productId,
+            'currency' => 'jpy',
+            'unit_amount' => $amount,
+            'recurring' => ['interval' => 'month'],
+            'metadata' => ['amount' => $amount],
+        ]);
     }
 
     /**
-     * @throws CustomException
+     * @throws ApiErrorException
+     * @throws InvalidRequestException
+     * @throws Exception
      */
     public static function searchPriceByPriceId(string $priceId)
     {
-        try {
-            $stripe = app(StripeClient::class);
-            $price = $stripe->prices->retrieve($priceId);
+        $stripe = app(StripeClient::class);
 
-            return $price;
-        } catch (Exception $e) {
-            throw new CustomException(
-                LogType::ERROR,
-                __LINE__,
-                '',
-                'Failed to search Stripe price',
-                $e
-            );
-        }
+        return $stripe->prices->retrieve($priceId);
     }
 
     /**
-     * @throws CustomException
+     * @throws ApiErrorException
+     * @throws InvalidRequestException
+     * @throws Exception
      */
     public static function createCheckoutSession(string $customerId, string $priceId, $paymentIntentMetaData): Session
     {
-        try {
-            $stripe = app(StripeClient::class);
-            $donor_name = json_encode($paymentIntentMetaData['donor_name']);
-            $donor_email = json_encode($paymentIntentMetaData['donor_email']);
-            $envHelper = new EnvHelpers;
+        $stripe = app(StripeClient::class);
+        $donor_name = json_encode($paymentIntentMetaData['donor_name']);
+        $donor_email = json_encode($paymentIntentMetaData['donor_email']);
+        $envHelper = new EnvHelpers;
 
-            return $stripe->checkout->sessions->create([
-                'success_url' => env('FRONT_END_URL')."/{$envHelper->getUrlByEnv('success')}?name={$donor_name}&email={$donor_email}",
-                'cancel_url' => env('FRONT_END_URL')."/.{$envHelper->getUrlByEnv('cancel')}?name={$donor_name}",
-                'ui_mode' => 'hosted',
-                'customer' => $customerId,
-                'payment_method_types' => ['card'],
-                'line_items' => [[
-                    'price' => $priceId,
-                    'quantity' => 1,
-                ]],
-                'automatic_tax' => ['enabled' => false],
-                'mode' => 'payment',
-                'payment_intent_data' => [
-                    'metadata' => $paymentIntentMetaData,
-                ],
-            ]);
-        } catch (Exception $e) {
-            throw new CustomException(
-                LogType::FATAL,
-                __LINE__,
-                $paymentIntentMetaData['donor_email'],
-                'Failed to create Stripe checkout session',
-                $e
-            );
-        }
-
+        return $stripe->checkout->sessions->create([
+            'success_url' => env('FRONT_END_UR')."/{$envHelper->getUrlByEnv('success')}?name={$donor_name}&email={$donor_email}",
+            'cancel_url' => env('FRONT_END_URL')."/.{$envHelper->getUrlByEnv('cancel')}?name={$donor_name}",
+            'ui_mode' => 'hosted',
+            'customer' => $customerId,
+            'payment_method_types' => ['card'],
+            'line_items' => [[
+                'price' => $priceId,
+                'quantity' => 1,
+            ]],
+            'automatic_tax' => ['enabled' => false],
+            'mode' => 'payment',
+            'payment_intent_data' => [
+                'metadata' => $paymentIntentMetaData,
+            ],
+        ]);
     }
 
     /**
-     * @throws CustomException
+     * @throws ApiErrorException
+     * @throws InvalidRequestException
+     * @throws Exception
      */
     public static function createSubscriptionSession(string $customerId, string $priceId, $subscriptionDataMetaData): Session
     {
-        try {
-            $stripe = app(StripeClient::class);
-            $donor_name = json_encode($subscriptionDataMetaData['donor_name']);
-            $donor_email = json_encode($subscriptionDataMetaData['donor_email']);
-            $envHelper = new EnvHelpers;
+        $stripe = app(StripeClient::class);
+        $donor_name = json_encode($subscriptionDataMetaData['donor_name']);
+        $donor_email = json_encode($subscriptionDataMetaData['donor_email']);
+        $envHelper = new EnvHelpers;
 
-            return $stripe->checkout->sessions->create([
-                'success_url' => env('FRONT_END_URL')."/{$envHelper->getUrlByEnv('success')}?name={$donor_name}&email={$donor_email}",
-                'cancel_url' => env('FRONT_END_URL')."/.{$envHelper->getUrlByEnv('cancel')}?name={$donor_name}",
-                'ui_mode' => 'hosted',
-                'customer' => $customerId,
-                'payment_method_types' => ['card'],
-                'line_items' => [[
-                    'price' => $priceId,
-                    'quantity' => 1,
-                ]],
-                'automatic_tax' => ['enabled' => false],
-                'mode' => 'subscription',
-                'subscription_data' => [ // 注意: Subscription table に保存するため
-                    'metadata' => $subscriptionDataMetaData,
-                ],
-            ]);
-        } catch (Exception $e) {
-            throw new CustomException(
-                LogType::FATAL,
-                __LINE__,
-                $subscriptionDataMetaData['donor_email'],
-                'Failed to create Stripe checkout session',
-                $e
-            );
-        }
+        return $stripe->checkout->sessions->create([
+            'success_url' => env('FRONT_END_URL')."/{$envHelper->getUrlByEnv('success')}?name={$donor_name}&email={$donor_email}",
+            'cancel_url' => env('FRONT_END_URL')."/.{$envHelper->getUrlByEnv('cancel')}?name={$donor_name}",
+            'ui_mode' => 'hosted',
+            'customer' => $customerId,
+            'payment_method_types' => ['card'],
+            'line_items' => [[
+                'price' => $priceId,
+                'quantity' => 1,
+            ]],
+            'automatic_tax' => ['enabled' => false],
+            'mode' => 'subscription',
+            'subscription_data' => [ // 注意: Subscription table に保存するため
+                'metadata' => $subscriptionDataMetaData,
+            ],
+        ]);
     }
 
     /**
-     * @throws CustomException
+     * @throws ApiErrorException
+     * @throws InvalidRequestException
+     * @throws Exception
      */
     public static function getProductNameFromId(string $productId): string
     {
-        try {
-            $stripe = app(StripeClient::class);
-            $product = $stripe->products->retrieve($productId);
+        $stripe = app(StripeClient::class);
 
-            return $product->name;
-        } catch (Exception $e) {
-            throw new CustomException(
-                LogType::FATAL,
-                __LINE__,
-                '',
-                'Failed to get Stripe product name',
-                $e
-            );
-        }
+        return $stripe->products->retrieve($productId)->name;
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Services\DiscordService;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Stripe\Exception\ApiErrorException;
+use Stripe\Exception\InvalidRequestException;
+use Stripe\Exception\SignatureVerificationException;
+use Stripe\Exception\UnexpectedValueException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +22,75 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+
+        $exceptions->render(function (SignatureVerificationException $e, Request $request) {
+            DiscordService::sendErrorMessage($e, 'Stripe API Webhook error. Invalid signature error occurred.');
+
+            return response()->json([
+                'message' => 'Stripe API Webhook error. Invalid signature error occurred.',
+                'error' => $e->getMessage(),
+                'request' => $request->all(),
+            ], 400);
+        });
+
+        $exceptions->render(function (UnexpectedValueException $e, Request $request) {
+            DiscordService::sendErrorMessage($e, 'Stripe API Webhook error. Invalid payload error occurred.');
+
+            return response()->json([
+                'message' => 'Stripe API Webhook error. Invalid payload error occurred.',
+                'error' => $e->getMessage(),
+                'request' => $request->all(),
+            ], 400);
+        });
+
+        $exceptions->render(function (InvalidRequestException $e, Request $request) {
+            DiscordService::sendErrorMessage($e, 'Stripe API Webhook error. Invalid payload error occurred.');
+
+            return response()->json([
+                'message' => 'Stripe API error. Invalid payload error occurred.',
+                'error' => $e->getMessage(),
+                'request' => $request->all(),
+            ], 400);
+        });
+
+        $exceptions->render(function (ApiErrorException $e, Request $request) {
+            DiscordService::sendErrorMessage($e, 'Stripe API Webhook error. Api error occurred.');
+
+            return response()->json([
+                'message' => 'Stripe API error. Api error occurred.',
+                'error' => $e->getMessage(),
+                'request' => $request->all(),
+            ], 400);
+        });
+
+        $exceptions->render(function (\Google\Service\Exception $e, Request $request) {
+            DiscordService::sendErrorMessage($e, 'Google Drive API error. Api error occurred.');
+
+            return response()->json([
+                'message' => 'Google Drive API error. Api error occurred.',
+                'error' => $e->getMessage(),
+                'request' => $request->all(),
+            ], 400);
+        });
+
+        $exceptions->render(function (ValidationException $e, Request $request) {
+            DiscordService::sendErrorMessage($e, 'Validation error. Invalid request error occurred.');
+
+            return response()->json([
+                'message' => 'Validation error. Invalid request error occurred.',
+                'error' => $e->errors(),
+                'request' => $request->all(),
+            ], 422);
+        });
+
+        $exceptions->render(function (Exception $e, Request $request) {
+            DiscordService::sendErrorMessage($e, 'Internal server error. An unexpected error occurred');
+
+            return response()->json([
+                'message' => 'Internal server error. An unexpected error occurred',
+                'error' => $e->getMessage(),
+                'request' => $request->all(),
+            ], 500);
+        });
+
     })->create();
