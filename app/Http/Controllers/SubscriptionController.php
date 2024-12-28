dda8c244328ca8312771ccdeff0292cbfcb42199
Bishwas Koirala <wasubisu69@gmail.com>
2024-12-28 23:44:10 +0900

    todo 生理

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -15,6 +15,10 @@ class SubscriptionController extends Controller
      */
     public function deleteSubscription($subscription_id): JsonResponse
     {
+        // TODO: external ID を受け取って、 subscription_ID db から取る。
+        // TODO: 撮った subscripton id を stripeProvider::cancelSubscriptionに叩く。
+        // TODO: レスポンスを元に, subscription  table で　is_cancelledを1に変える。
+        // TODO: レスポンスを元に userにキャンセル完了メールを送る。
         $cancelled_subscription = StripeProvider::cancelSubscription($subscription_id);
 
         //TODO: このデータを元に、管理者にメールにて通知する。
