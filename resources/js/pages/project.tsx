4385477dedc868cfb9ac4ab38af151d9f9214a84
Rajan Valencia <47655366+rajanvalencia@users.noreply.github.com>
2025-01-03 04:27:34 +0900

    feat: project page

--- resources/js/pages/project.tsx
+++ resources/js/pages/project.tsx
@@ -1,7 +1,13 @@
+import Layout from "@/layout";
+
 type Props = {
     project: string;
 };
 
 export default function Project({ project }: Props) {
-    return <h1>{project}</h1>;
+    return (
+        <Layout className="p-2 mx-auto max-w-screen-2xl">
+            <h1 className="text-3xl font-bold text-primary">{project}</h1>
+        </Layout>
+    );
 }
