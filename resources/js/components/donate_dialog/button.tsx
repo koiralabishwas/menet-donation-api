652a0ceb115151ed5e2969963920669ae7e59569
Rajan Valencia <47655366+rajanvalencia@users.noreply.github.com>
2025-01-04 01:55:59 +0900

    reafctor: button

---
+++ resources/js/components/donate_dialog/button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Button() {
+    return (
+        <div className="flex justify-center p-1">
+            <button className="w-32 h-full text-xl font-bold text-white rounded-lg bg-primary">
+                寄付する
+            </button>
+        </div>
+    );
+}
