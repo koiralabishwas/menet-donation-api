a0a2224444229f86e0a8695820b945d1cf972471
Rajan Valencia <47655366+rajanvalencia@users.noreply.github.com>
2025-01-03 23:39:11 +0900

    feat: header

--- resources/js/layout/header.tsx
+++ resources/js/layout/header.tsx
@@ -1,7 +1,16 @@
 export default function Header() {
     return (
-        <header className="h-16 bg-white border-b border-neutral-light">
-            <h1>ME-net</h1>
+        <header className="flex items-center h-16 bg-white border-b xl:h-24 border-neutral-light dark:bg-neutral">
+            <img
+                src="/svg/logo.svg"
+                alt="ME-net Logo"
+                className="w-auto h-full dark:hidden"
+            />
+            <img
+                src="/svg/logo_dark.svg"
+                alt="ME-net Logo"
+                className="hidden w-auto h-full dark:block"
+            />
         </header>
     );
 }
