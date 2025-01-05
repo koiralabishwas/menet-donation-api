9ceb929d578ac46ab3f791bfb68da6c5bf00fc2a
Rajan Valencia <47655366+rajanvalencia@users.noreply.github.com>
2025-01-06 07:43:46 +0900

    feat: vite asset config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            input: ["resources/css/app.css", "resources/js/app.jsx"],
             refresh: true,
         }),
-        react(),
     ],
+    base: process.env.ASSET_URL || "/",
 });
